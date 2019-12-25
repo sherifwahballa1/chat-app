44cb778ac54a7efa7f3df75a50ee7b51152d2f12
sherifwahballa1 <sherifwahballa0@gmail.com>
2019-12-25 23:33:26 +0200

    'emit-newMessage-on-createMessage'

--- public/js/index.js
+++ public/js/index.js
@@ -3,21 +3,7 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-//   socket.emit('createEmail', {
-//     to: 'jen@example.com',
-//     text: 'Hey. This is Andrew.'
-//   });
-
-  socket.emit('createMessage', {
-      from: 'sherif',
-      text: "Yup, that works for me"
-  });
-
 });
-
-// socket.on('newEmail', function (email) {
-//     console.log('New email', email);
-//   });
 
  socket.on('newMessage', function(message){
      console.log('newMessage', message);

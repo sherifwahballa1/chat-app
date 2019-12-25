97dee275252eff9396275a4d6ef107923f182add
sherifwahballa1 <sherifwahballa0@gmail.com>
2019-12-25 23:54:20 +0200

    'great-new-user,-and-alert-others'

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,12 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
 });
-
- socket.on('newMessage', function(message){
-     console.log('newMessage', message);
- })
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
 });

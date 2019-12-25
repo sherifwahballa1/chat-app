d5fb938c25fb67d6d8eaba86a87f5e00a29f7276
sherifwahballa1 <sherifwahballa0@gmail.com>
2019-12-25 21:17:32 +0200

    'init2_commit'

--- server/server.js
+++ server/server.js
@@ -8,5 +8,5 @@ app.use(express.static(publicPath));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is up on ${port}`);
+    console.log(`Server is up on ${port}`);
 });
